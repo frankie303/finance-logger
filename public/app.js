db4da0e1b54a470117033a58af3a15dd8eb7a3c3
mert <uzeyirmertciflikli@gmail.com>
2021-02-21 20:42:07 +0100

    add interfaces to classes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Payment } from './classes/Payment.js';
 const form = document.querySelector('.new-item-form');
 //inputs
 const type = document.querySelector('#type');
@@ -15,5 +8,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
